e6a68219e0535a30258e45a617f2bca05ea89355
ibambaya01 <ibtambaya01@gmail.com>
2023-07-17 09:47:35 -0700

    push print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return:  always 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
-		return (0):
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
